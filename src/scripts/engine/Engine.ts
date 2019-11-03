b402364cf2ddf512c4eb215a393cfbfe1226cb4d
Timotej <timotej.manfreda@gmail.com>
2019-11-03 01:40:11 +0100

    Draw a simple triangle - only temporary

--- src/scripts/engine/Engine.ts
+++ src/scripts/engine/Engine.ts
@@ -5,17 +5,32 @@ import * as BasicShader from './shaders/Basic';
 
 export class Engine {
     private _canvas: HTMLCanvasElement;
+    private _buffer: WebGLBuffer;
 
     public constructor(elementId: string) {
         this._canvas = WebGL.initialize(elementId);
         window.onresize = this.resize;
+        this.resize();
     }
 
     public start() {
         gl.clearColor(0, 0, 0, 1);
-        
+
         const basicShader = new Shader(BasicShader.vertexSource, BasicShader.fragmentSource);
         basicShader.use();
+
+        // Draw triangle (temporary)
+        this._buffer = gl.createBuffer();
+        const vertices = [
+            -0.5, -0.5, 0,
+            0, 0.5, 0,
+            0.5, -0.5, 0
+        ];
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, undefined);
+        //
 
         this.loop();
     }
@@ -31,6 +46,18 @@ export class Engine {
     private loop() {
         // Clear the color buffer of the screen, otherwise we draw each frame on top of each other.
         gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Draw triangle (temporary)
+        gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(0);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, undefined);
+        gl.disableVertexAttribArray(0);
+        //
+
         requestAnimationFrame(this.loop.bind(this));
     }
 }
--- src/scripts/engine/shaders/Basic.ts
+++ src/scripts/engine/shaders/Basic.ts
@@ -1,15 +1,22 @@
-export const vertexSource = `
-attribute vec3 aPosition;
+export const vertexSource = `#version 300 es
+in vec3 position;
+
+out vec4 color;
 
 void main() {
-    gl_Position = vec4(aPosition, 1.0);
+    color = vec4(abs((position.xy - 1.0) / 2.0), 0.0, 1.0);
+    gl_Position = vec4(position, 1.0);
 }
 `;
 
-export const fragmentSource = `
+export const fragmentSource = `#version 300 es
 precision mediump float;
 
+in vec4 color;
+
+out vec4 outColor;
+
 void main() {
-    gl_FragColor = vec4(1.0);
+    outColor = color;
 }
 `;
